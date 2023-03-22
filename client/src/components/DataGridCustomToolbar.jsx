30263bd3ef7a1ebfa5c327433b3db19896647603
UtkarshSonkar <utkarshsonkar8@gmail.com>
2023-03-22 19:35:07 +0530

    My initial commit

---
+++ client/src/components/DataGridCustomToolbar.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  GridToolbarContainer,
+  GridToolbarExport,
+  GridToolbarDensitySelector,
+  GridToolbarColumnsButton,
+} from "@mui/x-data-grid";
+import FlexBetween from "./FlexBetween";
+import { IconButton, InputAdornment, TextField } from "@mui/material";
+import { Search } from "@mui/icons-material";
+
+const DataGridCustomToolbar = ({ searchInput, setSearchInput, setSearch }) => {
+  return (
+    <GridToolbarContainer>
+      <FlexBetween width="100%">
+        <FlexBetween>
+          <GridToolbarColumnsButton />
+          <GridToolbarDensitySelector />
+          <GridToolbarExport />
+        </FlexBetween>
+        <TextField
+          label="Search..."
+          sx={{ mb: "0.5rem", width: "15rem" }}
+          variant="standard"
+          onChange={(e) => setSearchInput(e.target.value)}
+          value={searchInput}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton
+                  onClick={() => {
+                    setSearch(searchInput);
+                    setSearchInput("");
+                  }}
+                >
+                  <Search />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </FlexBetween>
+    </GridToolbarContainer>
+  );
+};
+
+export default DataGridCustomToolbar;
